781d3edaf04f8cd7644d126ebe706bddeedb92e5
Poonyaritkwangthong <117970215+Poonyaritkwangthong@users.noreply.github.com>
2022-11-21 20:34:21 +0700

    Add files via upload

---
+++ next-01.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <script src="https://cdn.tailwindcss.com"></script>
+</head>
+<body>
+    <h1 class = "text-center">Welcome to being a man</h1>
+    <img class="float-none object-cover h-96 w-48 ..." src="/6414421006/work.02/strongman.jpg">
+    <iframe class="w-full aspect-video ..." src="https://youtu.be/hkUJv6wz0RI/...."></iframe>
+</body>
+</html>
